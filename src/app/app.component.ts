411abb1217627c602445462fddc56068ca575914
Ganesh Bendi <ganesh@Ganeshs-MacBook-Air.local>
2024-03-23 00:04:35 +0530

    Your commit message here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieListComponent } from "./movie-list/movie-list.component";
+import { ChoresListComponent } from "./chores-list/chores-list.component";
+import { FavPhotosComponent } from "./fav-photos/fav-photos.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MovieListComponent, ChoresListComponent, FavPhotosComponent]
 })
 export class AppComponent {
   title = 'Day2_Assignment';
